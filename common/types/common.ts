aca575e57adfc2a38066ff71609187283e509ba0
gimpon <dbez@outlook.com>
2021-02-24 11:53:22 +0200

    matchOperatorByType

--- common/types/common.ts
+++ common/types/common.ts
@@ -1,10 +1,11 @@
 export type matchOperator = '=' | '<>' | '>=' | '<=' | '<' | '>' | 'like' | 'not like' | 'in' | 'not in' | 'beetwen' | 'is null' | 'is not null';
 
 export const matchOperatorByType: { [x: string]: matchOperator[] } = {
-    string: ['=', 'like', 'not like'],
-    enum: ['=', 'in'],
+    string: ['like', '=', 'not like'],
     number: ['=', '<=', '<', '>', '>=', 'beetwen'],
     date: ['beetwen', '=', '<=', '<', '>', '>=', '='],
     datetime: ['beetwen', '<=', '<', '>', '>=', '='],
+    enum: ['=', 'in'],
+    boolean: ['='],
     default: ['=', 'in', 'not in', 'is null', 'is not null']
 }
--- dist/common/types/common.js
+++ dist/common/types/common.js
@@ -2,11 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.matchOperatorByType = void 0;
 exports.matchOperatorByType = {
-    string: ['=', 'like', 'not like'],
-    enum: ['=', 'in'],
+    string: ['like', '=', 'not like'],
     number: ['=', '<=', '<', '>', '>=', 'beetwen'],
     date: ['beetwen', '=', '<=', '<', '>', '>=', '='],
     datetime: ['beetwen', '<=', '<', '>', '>=', '='],
+    enum: ['=', 'in'],
+    boolean: ['='],
     default: ['=', 'in', 'not in', 'is null', 'is not null']
 };
 //# sourceMappingURL=common.js.map

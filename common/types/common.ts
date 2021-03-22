e971c9482e8c9b45f6405e353cac2a65e539b451
gimpon <dbez@outlook.com>
2021-03-22 10:14:52 +0200

    in group

--- common/types/common.ts
+++ common/types/common.ts
@@ -1,4 +1,4 @@
-export type matchOperator = '=' | '<>' | '>=' | '<=' | '<' | '>' | 'like' | 'not like' | 'in' | 'not in' | 'beetwen' | 'is null' | 'is not null' | 'start with' | 'end with' | 'matching' | 'don\'t matching';
+export type matchOperator = '=' | '<>' | '>=' | '<=' | '<' | '>' | 'like' | 'not like' | 'in group' | 'not in group' | 'in' | 'not in' | 'beetwen' | 'is null' | 'is not null' | 'start with' | 'end with' | 'matching' | 'don\'t matching';
 
 export const matchOperatorByType: { [x: string]: matchOperator[] } = {
     string: ['like', 'not like', '=', 'start with', 'end with', 'matching', 'don\'t matching'],
